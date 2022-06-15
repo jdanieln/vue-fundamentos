0223d1c849f7258dc7a3c6c37d7237cb98ddbc6a
Jose Narvaez <josedanielnarvaez452@gmail.com>
2022-06-14 20:56:57 -0600

    addingVuetity

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,16 @@
 <template>
-  <div class="home">
-    <router-view/>
-  </div>
+  <hello-world />
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+  import Vue from 'vue'
+  import HelloWorld from '../components/HelloWorld.vue'
 
-export default Vue.extend({
-  name: 'HomeView',
-  components: {
-  },
-  data: () => ({
-    message: 'Welcome to Your Vue.js + TypeScript App, Message from HomeView Data'
-  }),
-  methods: {
-    handleHelloMessage(param: string) {
-      console.log("SE HA RECIBIDO UN EVENTO", param);
-    }
-  }
-});
+  export default Vue.extend({
+    name: 'HomeView',
+
+    components: {
+      HelloWorld,
+    },
+  })
 </script>
